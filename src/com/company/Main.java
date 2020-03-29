c7f51bc7083984ab78777769107c8f50a0b6b916
Vincer0 <pasierb.adam@gmail.com>
2020-03-29 11:36:41 +0200

    refactor and reformat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,34 +2,26 @@ package com.company;
 
 import java.util.Scanner;
 
-import static java.lang.String.format;
-
 public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        String aString = "";
 
         System.out.println("Podaj dwie kolejne wartosci liczbowe");
         int variableOne = input.nextInt();
         int variableTwo = input.nextInt();
 
         boolean isEqual = variableOne == variableTwo;
-        aString = String.format("%d == %d: %s", variableOne, variableTwo, isEqual);
-        System.out.println(aString);
+        System.out.println(String.format("%d > %d: %s", variableOne, variableTwo, isEqual));
 
         boolean isNotEqual = variableOne != variableTwo;
-        aString = String.format("%d != %d: %s", variableOne, variableTwo, isNotEqual);
-        System.out.println(aString);
+        System.out.println(String.format("%d > %d: %s", variableOne, variableTwo, isNotEqual));
 
         boolean isBiggerThanTwo = variableOne > variableTwo;
-        aString = String.format("%d > %d: %s", variableOne, variableTwo, isBiggerThanTwo);
-        System.out.println(aString);
+        System.out.println(String.format("%d > %d: %s", variableOne, variableTwo, isBiggerThanTwo));
 
         boolean isLessOrEqualToOne = variableTwo <= variableOne;
-        aString = String.format("%d <= %d: %s", variableOne, variableTwo, isLessOrEqualToOne);
-        System.out.println(aString);
+        System.out.println(String.format("%d > %d: %s", variableOne, variableTwo, isLessOrEqualToOne));
 
 
     }
