809938a22ff5fde6e1288f3da366a90f71bb7ea2
Vincer0 <pasierb.adam@gmail.com>
2020-03-29 11:11:07 +0200

    added String.format() to concat strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
+import static java.lang.String.format;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        String aString = "";
+
+        System.out.println("Podaj dwie kolejne wartosci liczbowe");
+        int variableOne = input.nextInt();
+        int variableTwo = input.nextInt();
+
+        boolean isEqual = variableOne == variableTwo;
+        aString = String.format("%d == %d: %s", variableOne, variableTwo, isEqual);
+        System.out.println(aString);
+
+        boolean isNotEqual = variableOne != variableTwo;
+        aString = String.format("%d != %d: %s", variableOne, variableTwo, isNotEqual);
+        System.out.println(aString);
+
+        boolean isBiggerThanTwo = variableOne > variableTwo;
+        aString = String.format("%d > %d: %s", variableOne, variableTwo, isBiggerThanTwo);
+        System.out.println(aString);
+
+        boolean isLessOrEqualToOne = variableTwo <= variableOne;
+        aString = String.format("%d <= %d: %s", variableOne, variableTwo, isLessOrEqualToOne);
+        System.out.println(aString);
+
+
     }
 }
