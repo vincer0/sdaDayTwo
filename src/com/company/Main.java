323775b1d68e35eaf0c0d6704cf333e92bba354c
Vincer0 <pasierb.adam@gmail.com>
2020-03-29 18:03:29 +0200

    added switch() instruction, added SimpleCalculator app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Witaj, wybierz temat rozmowy z konsultantem");
+        int opcja = input.nextInt();
+
+        switch (opcja) {
+            case 1:
+                System.out.println("Nowa oferta operatora");
+                break;
+            case 2:
+                System.out.println("Obsługa biezacego rachunku");
+                break;
+            case 3:
+                System.out.println("Usługi dodatkowe");
+                break;
+            case 4:
+                System.out.println("Reklamacje");
+                break;
+            case 9:
+                System.out.println("Kontakt bezposrednio z konsultantem");
+                break;
+            default:
+                System.out.println("Błędna opcja, wybierz inną");
+        }
     }
 }
--- src/com/company/SimpleCalculator.java
+++ src/com/company/SimpleCalculator.java
@@ -1,7 +1,60 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class SimpleCalculator {
     public static void main(String[] args) {
+        /**
+         * Zadanie 6
+         */
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Program wykonuje podstawowe obliczenia matematyczne...");
+
+        char enter;
+
+        do {
+            System.out.println("Podaj pierwszą liczbę");
+            float firstNumber = input.nextFloat();
+
+            System.out.println("Podaj symbol operacji matematycznej: (+, -, *, /)");
+            char operator = input.next().charAt(0);
+
+            float secondNumber;
+            float result = 0;
+
+            switch (operator) {
+                case '+':
+                    System.out.println("Podaj druga liczbe: ");
+                    secondNumber = input.nextFloat();
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    System.out.println("Podaj druga liczbe: ");
+                    secondNumber = input.nextFloat();
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    System.out.println("Podaj druga liczbe: ");
+                    secondNumber = input.nextFloat();
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    System.out.println("Podaj druga liczbe: ");
+                    secondNumber = input.nextFloat();
+                    while (secondNumber == 0) {
+                        System.out.println("Podales 0 nie wolno dzielic przez zero podaj inna liczbe");
+                        secondNumber = input.nextFloat();
+                    }
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    System.out.println("Podales zly operator! Koncze");
+                    return;
+            }
+            System.out.println("Wynik: " + result);
+            System.out.println("Czy chcesz kontynuować? T/N");
+            enter = input.next().charAt(0);
+        } while (enter != 'n');
     }
 }
